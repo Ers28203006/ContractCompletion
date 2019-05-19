c598fe02c69985e116b11068a6853bd51801d8df
Ers28203006 <ersyn_sultanbaev@mail.ru>
2019-05-19 14:03:09 +0600

    Добавьте файлы проекта.

---
+++ ContractCompletion/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ContractCompletion
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (tenantTextBlock.Text == "" || issuingAuthorityTextBlock.Text == "" || passportNumberTenantTextBlock.Text == ""
+                || passportSeriesTenantTextBlock.Text == "" || dateTextBlock.Text == "")
+            {
+                MessageBox.Show("Договор заполнен не в полном объеме");
+            }
+            else
+            {
+                PrintDialog print = new PrintDialog();
+                if (print.ShowDialog() == true)
+                {
+                    print.PrintDocument(((IDocumentPaginatorSource)flowDocumentScrollViewer.Document).DocumentPaginator, "A flow Document");
+                }
+            }
+        }
+    }
+}
